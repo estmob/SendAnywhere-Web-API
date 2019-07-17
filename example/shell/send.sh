9f25b4cc0cf683f13bf6c612d86f1f458139f35c
yth0625 <yth0625@gmail.com>
2019-07-17 16:43:55 +0900

    Update send script

--- example/shell/send.sh
+++ example/shell/send.sh
@@ -1,4 +1,17 @@
-#! /bin/sh
+#! /bin/bash
+for FILE in $*; do
+    if [ ! -e $FILE ]; then
+        echo "$FILE does not exist. Please check the arguments you entered.";
+        exit;
+    fi  
+done
+
+dpkg -s jq 1> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "jq package is not installed! Please install jq (sudo apt install jq)"
+    exit;
+fi
 
 . ./auth.sh
 
@@ -9,7 +22,20 @@ if `test $# -lt 1`; then
     exit 1
 fi
 
-RET=`curl -s -b$TOKEN_FILE https://send-anywhere.com/web/v1/key`
+BODY_OBJECT='{"file":[';
+for FILE in $*; do
+    BODY_OBJECT=$BODY_OBJECT'{"name":"'$FILE'","size":'$(stat -c%s $FILE)'}',
+done
+BODY_OBJECT=${BODY_OBJECT::-1}
+BODY_OBJECT=$BODY_OBJECT']}'
+
+echo $BODY_OBJECT
+
+RET=`curl -v -XPOST -b$TOKEN_FILE \
+	 -H 'Content-Type: application/json' \
+	  -d $BODY_OBJECT \
+	   'https://send-anywhere.com/web/v1/key'`
+
 
 KEY=`echo $RET | jq '.key' -r 2>/dev/null`
 FILE_URL=`echo $RET | jq '.weblink' -r 2>/dev/null`
@@ -20,6 +46,10 @@ if `test -z "$KEY"`; then
 fi
 
 echo "key: $KEY"
-curl -F file=@$1 $FILE_URL > /dev/null
 
+FILE_UPLOAD='';
+for FILE in $*; do
+    FILE_UPLOAD=$FILE_UPLOAD"-F file=@$FILE "
+done;
 
+curl $FILE_UPLOAD $FILE_URL > /dev/null
